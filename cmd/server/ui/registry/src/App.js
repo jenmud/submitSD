aa2d1847841b3a61c8fed331dc4f2bac98f1391c
Jenda Mudron <jenmud@gmail.com>
2021-02-22 10:31:44 +1100

    adding in the table

--- cmd/server/ui/registry/src/App.js
+++ cmd/server/ui/registry/src/App.js
@@ -1,11 +1,29 @@
 import 'fomantic-ui-css/semantic.css';
-import { Container, Header } from 'semantic-ui-react';
+import { Container, Header, Table } from 'semantic-ui-react';
 
 function App() {
   return (
     <div className="App">
       <Container>
         <Header>Registry</Header>
+
+        <Table celled>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>UID</Table.HeaderCell>
+              <Table.HeaderCell>Name</Table.HeaderCell>
+              <Table.HeaderCell>Address</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            <Table.Row>
+              <Table.Cell>demo-uid-1234</Table.Cell>
+              <Table.Cell>Demo.srv</Table.Cell>
+              <Table.Cell>localhost:1234</Table.Cell>
+            </Table.Row>
+          </Table.Body>
+        </Table>
+
       </Container>
     </div>
   );
