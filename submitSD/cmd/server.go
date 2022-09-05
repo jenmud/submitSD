a05062660f7fdd46bf0df227b225c1344167b41a
Jenda Mudron <jenmud@gmail.com>
2022-09-05 20:22:02 +1000

    updated the command line args

--- submitSD/cmd/server.go
+++ submitSD/cmd/server.go
@@ -1,6 +1,5 @@
 /*
 Copyright © 2022 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
@@ -13,13 +12,9 @@ import (
 // serverCmd represents the server command
 var serverCmd = &cobra.Command{
 	Use:   "server",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "server is a registry which is used for storing services.",
+	Long: `server is a registry which is used for storing services and
+querying for services.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("server called")
 	},
